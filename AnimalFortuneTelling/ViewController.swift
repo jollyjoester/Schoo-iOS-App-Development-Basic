28587e0af30209a712cad04f6abf7d75b727880e
jollyjoester <jollyjoester@gmail.com>
2016-03-04 14:13:28 +0900

    Fix: implement fortune telling

--- AnimalFortuneTelling/ViewController.swift
+++ AnimalFortuneTelling/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var animalLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tellFortunes(sender: AnyObject) {
 
+        let random = arc4random_uniform(6)
+
+        switch random {
+        case 0:
+            animalLabel.text = "🐶"
+            resultLabel.text = "大吉だワン！"
+        case 1:
+            animalLabel.text = "🐱"
+            resultLabel.text = "中吉だニャー！"
+        case 2:
+            animalLabel.text = "🐴"
+            resultLabel.text = "吉だヒヒーン！"
+        case 3:
+            animalLabel.text = "🐮"
+            resultLabel.text = "末吉だモー！"
+        case 4:
+            animalLabel.text = "🐘"
+            resultLabel.text = "凶だゾウ！"
+        default:
+            animalLabel.text = "🐯"
+            resultLabel.text = "大凶だガオー！"
+        }
+    }
 }
 
